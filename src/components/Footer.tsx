99649306b821467da4e9a28998de1426f855513c
35934it4-Shoaib <35934.it4@gmail.com>
2026-09-22 06:19:48 +0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -135,6 +135,11 @@ export const Footer: React.FC<FooterProps> = ({ onNavigate }) => {
                 </button>
               </li>
               <li>
+  <button onClick={() => onNavigate('/payment-security')} className="hover:text-[#4E6155] transition-colors text-left cursor-pointer">
+    Payment & Security
+  </button>
+</li>
+              <li>
                 <button onClick={() => onNavigate('/contact')} className="hover:text-[#4E6155] transition-colors text-left cursor-pointer">
                   Contact Us
                 </button>
